3415e4550d4be99dba0443844c106a13fe0dd1f9
Chamidu Shilakshitha <chamiduhp48@gmail.com>
2025-08-23 23:12:54 +0530

    env add

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -3,16 +3,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyDlOdB3ecGH3263ki_3t8SxzQilbjGGKyo",
-    authDomain: "newbie-quiz-2f639.firebaseapp.com",
-    projectId: "newbie-quiz-2f639",
-    storageBucket: "newbie-quiz-2f639.firebasestorage.app",
-    messagingSenderId: "433428004713",
-    appId: "1:433428004713:web:469fa885be07eb703047f8",
-    measurementId: "G-9JXC5VN3HQ"
-  };
-
+const firebaseConfig =   {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId:  process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+};
+  
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
